d87e1b7a1009e1726b011625bd479585172c0ea8
Madimetja Shika <mmjshika@gmail.com>
2018-04-17 22:07:06 +0200

    WIP on lifecycle hooks tests

--- test/unit/specs/Initialisation.spec.js
+++ test/unit/specs/Initialisation.spec.js
@@ -1,17 +1,74 @@
-import { mount } from 'vue-test-utils';
+import { mount, shallow } from 'vue-test-utils';
 import VuetifyGoogleAutocomplete from '../../../src/index';
 
-const propData = {
+const requiredPropData = {
+  id: 'map',
 };
 let wrapper = null;
 beforeEach(() => {
   wrapper = mount(VuetifyGoogleAutocomplete, {
-    propsData: propData,
+    propsData: requiredPropData,
   });
 });
 
 describe('Ensure Component is initialised properly', () => {
   test('Is a Vue instance', () => {
-    expect(1).toBeTruthy();
+    expect(wrapper.isVueInstance()).toBeTruthy();
   });
+});
+
+describe('Lifecycle Hooks', () => {
+  describe('Created', () => {
+    test('Sets autocomplete text properly when given v-model.', () => {
+      const autocompleteText = 'Some autocomplete text.';
+
+      wrapper = mount(VuetifyGoogleAutocomplete, {
+        propsData: {
+          ...requiredPropData,
+          value: autocompleteText,
+        },
+      });
+
+      expect(wrapper.vm.autocompleteText).toEqual(autocompleteText);
+    });
+
+    test('Sets autocomplete text properly when NOT given v-model.', () => {
+      wrapper = mount(VuetifyGoogleAutocomplete, {
+        propsData: {
+          ...requiredPropData,
+        },
+      });
+
+      expect(wrapper.vm.autocompleteText).toEqual('');
+    });
+  });
+
+  describe('Mounted', () => {
+    test('vgaMapState should have correct default value when Google Init callback has not yet been called.', () => {
+      expect(wrapper.vm.vgaMapState).toEqual({
+        initMap: false,
+      });
+    });
+  });
+
+  describe('Mounted', () => {
+    test('vgaMapState should have correct value when Google Init callback HAS been called.', () => {
+      // Mock out google maps.
+      window.google = {
+        maps: {
+          places: {
+            Autocomplete: class {},
+          },
+        },
+      };
+      window.initVGAMaps();
+
+      expect(wrapper.vm.vgaMapState).toEqual({
+        initMap: true,
+      });
+    });
+  });
+
+  // describe('Destroyed', () => {
+  // });
 });
